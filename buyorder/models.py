3db752454cdfdb9f48ac295f6a61628f9c5b84a5
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2024-12-13 18:44:38 +0330

    created models for Crypto, Price, and PricePerRial.

--- buyorder/models.py
+++ buyorder/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Crypto(models.Model):
+    name = models.CharField(max_length=100)
+    symbol = models.CharField(max_length=10)
+
+class Price(models.Model):
+    crypto = models.ForeignKey(Crypto, on_delete=models.CASCADE)
+    price_per_dollar = models.DecimalField(max_digits=10, decimal_places=2)
+
+class PricePerRial(models.Model):
+    currency = models.CharField(max_length=10)
+    per_rial = models.DecimalField(max_digits=10, decimal_places=2)
