f8c1dfd6aa604e4ef9d1961233b4657325de0143
Mojtaba Negahdari <contact.mojtaba.negahdari@gmail.com>
2024-12-13 18:56:40 +0330

    added basic test for BuyOrderView API.

--- buyorder/tests.py
+++ buyorder/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from rest_framework import status
 
-# Create your tests here.
+class BuyOrderAPITest(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_create_buy_order(self):
+        data = {
+            "currency_name": "bitcoin",
+            "num_of_cryptos": 5
+        }
+        response = self.client.post('/api/buyorder/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
